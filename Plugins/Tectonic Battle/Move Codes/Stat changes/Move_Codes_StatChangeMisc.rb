b4137dbe494f2e3eee15e1a692eaf695691a3ca5
RiptideHub <66923704+RiptideHub@users.noreply.github.com>
2025-08-28 21:25:22 -0700

    Adding the TRICK code
    To Do: AI effect stuff

--- Plugins/Tectonic Battle/Move Codes/Stat changes/Move_Codes_StatChangeMisc.rb
+++ Plugins/Tectonic Battle/Move Codes/Stat changes/Move_Codes_StatChangeMisc.rb
@@ -225,27 +225,106 @@ class PokeBattle_Move_AverageUserTargetOffenses < PokeBattle_Move
 end
 
 #===============================================================================
-# Swaps the user's speed and item with the target's(Ion Exchange)
+# Swaps the user's speed and item with the target's (Ion Exchange)
 #===============================================================================
 class PokeBattle_Move_SwitchUserTargetSpeedsItems < PokeBattle_Move
+    def pbMoveFailed?(user, _targets, show_message)
+        if @battle.wildBattle? && user.opposes? && !user.boss
+            @battle.pbDisplay(_INTL("But it failed, since this is a wild battle!")) if show_message
+            return true
+        end
+        return false
+    end
+
+    def pbFailsAgainstTarget?(user, target, show_message)
+        unless target.hasAnyItem?
+            if show_message
+                @battle.pbDisplay(_INTL("But it failed, since {1} doesn't have an item!", target.pbThis(true)))
+            end
+            return true
+        end
+        unless user.hasAnyItem?
+            @battle.pbDisplay(_INTL("But it failed, since {1} doesn't have an item!", user.pbThis(true))) if show_message
+            return true
+        end
+        if target.unlosableItem?(target.firstItem) ||
+           target.unlosableItem?(user.firstItem) ||
+           user.unlosableItem?(user.firstItem) ||
+           user.unlosableItem?(target.firstItem)
+            @battle.pbDisplay(_INTL("But it failed!")) if show_message
+            return true
+        end
+        if user.firstItem == :PEARLOFWISDOM
+             @battle.pbDisplay(_INTL("But it failed, since the Pearl of Fate cannot be exchanged!")) if show_message
+            return true
+        end
+        if target.hasActiveAbility?(:STICKYHOLD) && !@battle.moldBreaker
+            if show_message
+                @battle.pbShowAbilitySplash(target, ability)
+                @battle.pbDisplay(_INTL("But it failed to affect {1}!", target.pbThis(true)))
+                @battle.pbHideAbilitySplash(target)
+            end
+            return true
+        end
+        return false
+    end
+
     def pbEffectAgainstTarget(user, target)
         newUserSpeed   = target.base_speed
         newTargetSpeed = user.base_speed
         user.applyEffect(:BaseSpeed,newUserSpeed)
         target.applyEffect(:BaseSpeed,newTargetSpeed)
         @battle.pbDisplay(_INTL("{1} swapped its base speed stats with the target!", user.pbThis))
+        
+        oldUserItem = user.firstItem
+        oldUserItemName = getItemName(oldUserItem)
+        oldTargetItem = target.firstItem
+        oldTargetItemName = getItemName(target.firstItem)
+        user.removeItem(oldUserItem)
+        target.removeItem(oldTargetItem)
+        if @battle.stolenItemTurnsToDust?
+            @battle.pbDisplay(_INTL("{1}'s {2} turned to dust.", user.pbThis, oldUserItemName)) if oldUserItem
+            @battle.pbDisplay(_INTL("{1}'s {2} turned to dust.", target.pbThis, oldTargetItemName)) if oldTargetItem
+        elsif !user.opposes? && target.shouldStoreStolenItem?(oldTargetItem)
+            @battle.pbDisplay(_INTL("{1} switched items with its opponent!", user.pbThis))
+            target.setInitialItems(nil)
+            pbReceiveItem(oldTargetItem)
+            target.giveItem(oldUserItem)
+            @battle.pbDisplay(_INTL("{1} obtained {2}.", target.pbThis, oldUserItemName)) if oldUserItem
+            target.pbHeldItemTriggerCheck
+        else
+            user.giveItem(oldTargetItem)
+            target.giveItem(oldUserItem)
+            @battle.pbDisplay(_INTL("{1} switched items with its opponent!", user.pbThis))
+            @battle.pbDisplay(_INTL("{1} obtained {2}.", user.pbThis, oldTargetItemName)) if oldTargetItem
+            @battle.pbDisplay(_INTL("{1} obtained {2}.", target.pbThis, oldUserItemName)) if oldUserItem
+            user.pbHeldItemTriggerCheck
+            target.pbHeldItemTriggerCheck
+        end
     end
 
     def getEffectScore(user, target)
         userSpeed = target.base_speed
         targetSpeed = user.base_speed
+        score = 0
         if userSpeed < targetSpeed
-            return 120
+            score = 120
         elsif userSpeed > targetSpeed
-            return 80
+            score = -10
         else
-            return 0
+            score = 0
         end
+
+        if user.hasActiveItemAI?(%i[FLAMEORB POISONORB STICKYBARB IRONBALL])
+            score = 130
+        elsif user.hasActiveItemAI?(GameData::Item.getByFlag("ChoiceLocking"))
+            score = 100
+        elsif !user.firstItem && target.firstItem
+            if user.lastMoveUsed && GameData::Move.get(user.lastMoveUsed).function_code == "SwapItems" # Trick/Switcheroo
+                return score
+            end
+        end
+        return score
     end
 end
 
