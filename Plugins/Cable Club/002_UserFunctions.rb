38ee970381bcdfef733c3d9f04707053073d81e9
AlphaKretin <deewhydeeecks@gmail.com>
2025-09-14 05:16:55 +0930

    Fix behaviour when cancelling out of Class Lister

--- Plugins/Cable Club/002_UserFunctions.rb
+++ Plugins/Cable Club/002_UserFunctions.rb
@@ -14,7 +14,7 @@ def pbChangeOnlineTrainerType
   pbMessage(_INTL("What Trainer Class do you want to present to your opponents?"))
   index = GameData::TrainerType.cableClubClasses.index(old_trainer_type.id) || 0
   loop do
-    new_trainer_type_id = pbListScreen(_INTL("Choose a class"), CCTrainerTypeLister.new(index))
+    new_trainer_type_id = pbListScreen(_INTL("Choose a class"), CCTrainerTypeLister.new(index)) || old_trainer_type.id
     new_trainer_type = GameData::TrainerType.get(new_trainer_type_id)
     trainername=new_trainer_type.real_name
     if ['a','e','i','o','u'].include?(trainername[0,1].downcase)
