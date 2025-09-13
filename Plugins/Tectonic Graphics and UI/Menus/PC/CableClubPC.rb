82d8f2965bb91fbe15ce19bb4470f442bc0326e6
AlphaKretin <deewhydeeecks@gmail.com>
2025-09-13 15:14:14 +0930

    Streamline Cable Club customisation dialogue
    Remove character-like dialogue for the PC interface

--- Plugins/Tectonic Graphics and UI/Menus/PC/CableClubPC.rb
+++ Plugins/Tectonic Graphics and UI/Menus/PC/CableClubPC.rb
@@ -3,17 +3,32 @@ def pbCableClubPCMenu
   loop do
     command = pbMessage(_INTL("What do you want to do?"),[
         _INTL("Connect"),
-        _INTL("Customise Trainer Class"),
-        _INTL("Customise Win Message"),
-        _INTL("Customise Lose Message"),
+        _INTL("Customize"),
         _INTL("Cancel")
         ],-1,nil,command)
     case command
     when 0 then pbCableClub
-    when 1 then pbChangeOnlineTrainerType
-    when 2 then pbChangeOnlineWinText
-    when 3 then pbChangeOnlineLoseText
+    when 1 then pbCableClubCustomMenu
     else        break
     end
   end
 end
+
+def pbCableClubCustomMenu
+  command = 0
+  loop do
+    command = pbMessage(_INTL("What do you want to change?"),[
+        _INTL("Trainer Class"),
+        _INTL("Win Message"),
+        _INTL("Loss Message"),
+        _INTL("Cancel")
+        ],-1,nil,command)
+    case command
+    when 0 then pbChangeOnlineTrainerType
+    when 1 then pbChangeOnlineWinText
+    when 2 then pbChangeOnlineLoseText
+    else        break
+    end
+  end
+end
+
